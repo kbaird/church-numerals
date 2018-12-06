02954e0f03f58ee7b115684e1bba06ccd20cc76c
Kevin Baird <kevincbaird@gmail.com>
2018-12-05 21:28:28 -0800

    Add decode

--- lib/church.ex
+++ lib/church.ex
@@ -17,7 +17,26 @@ defmodule Church do
       :arg
   """
   def encode(0), do: fn arg -> arg end
+
   def encode(num) when is_integer(num) and num > 0 do
-    fn -> encode(num-1) end
+    fn -> encode(num - 1) end
   end
+
+  @doc """
+  Decode numerals in accordance with https://en.wikipedia.org/wiki/Church_encoding
+
+  ## Examples
+
+      iex> zero = Church.encode(0)
+      iex> Church.decode(zero)
+      0
+
+      iex> five = Church.encode(5)
+      iex> Church.decode(five)
+      5
+  """
+  def decode(fun) when is_function(fun), do: decode(fun, 0)
+
+  defp decode(fun, num) when is_function(fun, 1), do: num
+  defp decode(fun, num), do: decode(fun.(), num + 1)
 end
--- test/church_test.exs
+++ test/church_test.exs
@@ -19,6 +19,21 @@ defmodule ChurchTest do
     assert_is_encoded_zero(zero)
   end
 
+  test "decode 0" do
+    zero = Church.encode(0)
+    assert Church.decode(zero) == 0
+  end
+
+  test "decode 1" do
+    one = Church.encode(1)
+    assert Church.decode(one) == 1
+  end
+
+  test "decode 9999" do
+    high = Church.encode(9999)
+    assert Church.decode(high) == 9999
+  end
+
   defp assert_is_encoded_zero(zero) do
     assert is_function(zero)
     assert zero.(:arg) == :arg
