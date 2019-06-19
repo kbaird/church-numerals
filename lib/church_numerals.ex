4a686baaf07344b5363176cb0530657263aaf7f3
Kevin Baird <kevincbaird@gmail.com>
2019-06-19 10:45:37 -0700

    Formatting and guard change

--- lib/church_numerals.ex
+++ lib/church_numerals.ex
@@ -48,7 +48,10 @@ defmodule ChurchNumerals do
   def decode(fun) when is_function(fun), do: decode(fun, 0)
 
   defp decode(fun, num) when is_zero_church(fun), do: num
-  defp decode(fun, num), do: decode(fun.(), num + 1)
+
+  defp decode(fun, num) when is_pos_church(fun) do
+    decode(fun.(), num + 1)
+  end
 
   @doc """
   ## Examples
@@ -154,7 +157,9 @@ defmodule ChurchNumerals do
 
   ### PRIVATE FUNCTIONS ###
 
-  defp one_church?(fun) when is_function(fun, 0), do: is_zero_church(fun.())
+  defp one_church?(fun) when is_pos_church(fun) do
+    is_zero_church(fun.())
+  end
 
   defp recurse(result, _operand, steps_remaining, _step_fun)
        when is_pos_church(result) and is_zero_church(steps_remaining) do
