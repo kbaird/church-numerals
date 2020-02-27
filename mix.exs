ece17d3f3b86a8218bd37726bb729cd7b8cf8545
Kevin Baird <kevincbaird@gmail.com>
2020-02-27 15:55:17 -0800

    Add Credo

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule ChurchNumerals.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
       {:propcheck, "~> 1.1", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
