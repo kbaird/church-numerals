ae71e596a0fc1811c8e23f6bce303cc75f824a28
Kevin Baird <kevincbaird@gmail.com>
2021-07-13 16:22:27 -0700

    Upgrade dialyxir and propcheck

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule ChurchNumerals.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:propcheck, "~> 1.1", only: [:dev, :test]}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:propcheck, "~> 1.4", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
