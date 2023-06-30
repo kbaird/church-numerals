36211e15b2a2973baacf17ce9144977189cfa68f
Kevin Baird <kevincbaird@gmail.com>
2023-06-29 17:35:21 -0700

    Upgrade elixir, credo & dialyxir

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ChurchNumerals.MixProject do
       ],
       app: :church_numerals,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -33,8 +33,8 @@ defmodule ChurchNumerals.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:propcheck, "~> 1.4", only: [:dev, :test]}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
