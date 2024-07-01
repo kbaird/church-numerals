5380ea77098401b59c719022e38ce24063cbb9f9
Kevin Baird <kevincbaird@gmail.com>
2024-06-30 17:21:52 -0700

    Bump elixir 1.17.0 -> 1.17.1

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ChurchNumerals.MixProject do
       ],
       app: :church_numerals,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
