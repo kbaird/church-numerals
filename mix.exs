9540dc512b1d3ce48063768121cba84297ff7ac0
Kevin Baird <kevincbaird@gmail.com>
2025-10-16 17:08:15 -0700

    Bump erlang to 28.1 and elixir to 1.19

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule ChurchNumerals.MixProject do
       ],
       app: :church_numerals,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
